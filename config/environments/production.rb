c8a63f06f78c6b31f6a7be4ccefa0549424c3914
paulwallis42 <paulwallis42@gmail.com>
2017-02-08 12:25:11 +0000

    bootstrap tinkering

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,20 @@ Rails.application.configure do
   config.eager_load = true
 
   config.action_mailer.default_url_options = { :host => 'rocky_gorge-67246.herokuapp.com' }
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings =
+{
+
+  :address            => 'smtp.gmail.com',
+  :port               => 587,
+  :domain             => 'gmail.com'
+  :authentication     => :plain,
+  :user_name          => 'XXXXX@gmail.com',
+  :password           => 'XXXXXXX'
+}
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
